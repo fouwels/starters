e9e0cc2d2e011c8bf386fe1601a50867a3ff58ce
Kaelan Fouwels <kfouwels@outlook.com>
2015-06-29 13:47:18 +0100

    config all

--- src/proj/Startup.cs
+++ src/proj/Startup.cs
@@ -1,26 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
+using Microsoft.Framework.ConfigurationModel;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+using Microsoft.Framework.Logging.Console;
 
 namespace proj
 {
     public class Startup
     {
+		public IConfiguration Configuration { get; set; }
+		public Startup()
+		{
+			var configuration = new Configuration()
+			  .AddJsonFile("config.json");
+			//configuration.AddUserSecrets(); broken package on dnu
+
+			Configuration = configuration;
+		}
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddMvc();
+			services.AddLogging();
+
+			Debug.WriteLine("Configured Services - config check value:" + Configuration["data:IntendedConfigCheckValue"]); // Indentify accidental config file mis-match
         }
 
-        public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app, ILoggerFactory LoggerFactory)
         {
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+			LoggerFactory.AddConsole(LogLevel.Information);
+
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+			app.UseMvc();
+			app.UseErrorPage();
         }
     }
 }
