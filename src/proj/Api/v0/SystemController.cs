9a63b62930c8f9759fc723ff8f942ac92a6b25b6
Kaelan Fouwels <kfouwels@outlook.com>
2015-06-29 14:11:14 +0100

    controllers

---
+++ src/proj/Api/v0/SystemController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace proj.Api.v0
+{
+    [Route("api/v0/[controller]/[action]")]
+    public class SystemController : Controller
+    {
+		[HttpGet]
+		public object GetTime()
+		{
+			return new Dictionary<string, string> { { "time", DateTime.Now.ToString() } };
+		}
+    }
+}
