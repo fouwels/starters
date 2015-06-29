a0fa488993b0c19b18ca7afb1b6bd6f3c43fb9d4
Kaelan Fouwels <kfouwels@outlook.com>
2015-06-29 15:56:34 +0100

    Swagger + gulp-ey

--- src/proj/Gulpfile.js
+++ src/proj/Gulpfile.js
@@ -1,22 +1,37 @@
 ﻿var gulp = require('gulp');
 var merge = require('merge-stream');
 var rimraf = require("rimraf");
-
+var gutil = require('gulp-util');
+var gulpIgnore = require('gulp-ignore');
 
 gulp.task('swagger', function () {
-	var swaggerCore = 
-		gulp.src('/Swagger/Dist/**/*', '!index.html')
-		.pipe(gulp.dest('/wwwroot/'));
 
-	var swaggerIndex = 
-		gulp.src('/Swagger/Dist/index.html')
-		.pipe(gulp.dest('/wwwroot/documentation.html'));
+	var swagger =
+		gulp.src('./Swagger/Dist/**/*')
+		.pipe(gulpIgnore.exclude('./Swagger/Dist/index.html'))
+		.pipe(gulp.dest('./wwwroot/'));
 
-	return merge(swaggerCore, swaggerIndex);
+	var swagfile =
+		gulp.src('swagger.json')
+		.pipe(gulp.dest('./wwwroot/'));
+
+
+	//var swaggerIndex = 
+	//	gulp.src('./Swagger/Dist/index.html')
+	//	.pipe(rename('documentation.html'))
+	//	.pipe(gulp.dest('/wwwroot/'));
+
+	return merge(swagger, swagfile);
 });
 
-gulp.tas('clean', function (){
+gulp.task('clean', function (cb){
+	rimraf('./wwwroot/*', cb);
+});
 
-	rimraff('/wwwroot');
-
+gulp.task('default', function () {
+	gutil.log('\n\n************************************');
+	gutil.log('Available actions:');
+	gutil.log('clean: clean serving dir');
+	gutil.log('swagger: copy swagger files from dist into serving dir.');
+	gutil.log('\n*************************************');
 });
