3a512644538c77e94bc589febd4ccca9c9fac05d
Kaelan Fouwels <kae@kfwls.co>
2016-01-21 00:32:01 +0000

    gulpfile w/gulp-newer

--- web-atom/Gulpfile.js
+++ web-atom/Gulpfile.js
@@ -1,47 +1,52 @@
-﻿/*
-This file in the main entry point for defining Gulp tasks and using Gulp plugins.
-Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
-*/
-
 var gulp = require('gulp');
 var rimraf = require("rimraf");
+var newer = require('gulp-newer');
+
+var rootOutputDir = './wwwroot';
 
 gulp.task('clean', function (cb) {
-	rimraf('./wwwroot/*', cb);
+	rimraf(rootOutputDir + '/*', cb);
 });
 
 gulp.task('bootstrap', function () {
 	return gulp.src('./bower_components/bootstrap/dist/**/*')
-		.pipe(gulp.dest('./wwwroot/'));
+		.pipe(newer(rootOutputDir + '/'))
+		.pipe(gulp.dest(rootOutputDir + '/'));
 });
 
 gulp.task('jquery', function () {
 	return gulp.src('./bower_components/jquery/dist/**/*')
-		.pipe(gulp.dest('./wwwroot/js/'));
+		.pipe(newer(rootOutputDir + '/js/'))
+		.pipe(gulp.dest(rootOutputDir + '/js/'));
 });
 
 gulp.task('styles', function () {
 	return gulp.src('./dev/css/*')
-		.pipe(gulp.dest('./wwwroot/css/'));
+		.pipe(newer(rootOutputDir + '/css/'))
+		.pipe(gulp.dest(rootOutputDir + '/css/'));
 });
 
 gulp.task('scripts', function () {
 	return gulp.src('./dev/js/*')
-		.pipe(gulp.dest('./wwwroot/js/'));
+		.pipe(newer(rootOutputDir + '/js/'))
+		.pipe(gulp.dest(rootOutputDir + '/js/'));
 });
 
 gulp.task('pages', function () {
 	return gulp.src('./dev/*')
-		.pipe(gulp.dest('./wwwroot/'));
+		.pipe(newer(rootOutputDir + '/'))
+		.pipe(gulp.dest(rootOutputDir + '/'));
 });
 
 gulp.task('fonta-css', function () {
 	return gulp.src('./bower_components/font-awesome/css/*')
-		.pipe(gulp.dest('./wwwroot/css/'));
+		.pipe(newer(rootOutputDir + '/css/'))
+		.pipe(gulp.dest(rootOutputDir + '/css/'));
 });
 gulp.task('fonta-fonts', function () {
 	return gulp.src('./bower_components/font-awesome/fonts/*')
-		.pipe(gulp.dest('./wwwroot/fonts/'));
+		.pipe(newer(rootOutputDir + '/fonts/'))
+		.pipe(gulp.dest(rootOutputDir + '/fonts/'));
 });
 
 
@@ -52,4 +57,4 @@ gulp.task('watch', function () {
 });
 
 
-gulp.task('build', ['clean', 'bootstrap', 'jquery', 'styles', 'scripts', 'pages', 'fonta-css', 'fonta-fonts']);
+gulp.task('build', ['bootstrap', 'jquery', 'styles', 'scripts', 'pages', 'fonta-css', 'fonta-fonts']);
